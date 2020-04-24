a9d6bdecc27dea00e817c7d907bdfccc6c21bb62
Usman Khalid <usmankhan495@gmail.com>
2020-04-24 15:42:33 +0500

    intial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app=express()
+
+app.get('/',(req,res)=>{
+
+    res.send({
+        Hi:req.baseUrl
+    })
+})
+
+const PORT=process.env.PORT || 5000;
+
+app.listen(PORT);
